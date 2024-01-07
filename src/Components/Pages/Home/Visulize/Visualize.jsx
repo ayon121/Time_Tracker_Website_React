a8349ba0885e636b05eb07a0d3702c85d8980c95
ayon121 <borshonsaha1234@gmail.com>
2024-01-08 00:44:31 +0600

    visualizer updated

--- src/Components/Pages/Home/Visulize/Visualize.jsx
+++ src/Components/Pages/Home/Visulize/Visualize.jsx
@@ -4,8 +4,8 @@ import chart2 from '../../../../assets/animation/chart9.json'
 
 const Visualize = () => {
     return (
-        <div className="max-w-6xl mx-auto px-4 font-poppins">
-            <h1 className="text-xl md:text-3xl mt-3 text-center text-main">Visualize Your Progress</h1>
+        <div className="max-w-6xl mx-auto px-4 font-poppins mt-3">
+            <h1 className="text-2xl md:text-4xl mt-3 text-center text-main">Visualize Your Progress</h1>
             <div className="flex items-center justify-around h-4/5 border-2 mt-3 mb-5 rounded-3xl border-main">
                 {/* chart 1 */}
                 <div className="">
