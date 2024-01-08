b287471e32e63e674b15c5138f3b52de40ea032d
ayon121 <borshonsaha1234@gmail.com>
2024-01-09 02:11:59 +0600

    task description updated

--- src/Components/Pages/Dashboard/TaskDescription/TaskDescription.jsx
+++ src/Components/Pages/Dashboard/TaskDescription/TaskDescription.jsx
@@ -1,11 +1,25 @@
 
 // import PropTypes from 'prop-types';
+import { FaCalendarAlt } from "react-icons/fa";
+import { IoIosTime } from "react-icons/io";
 
 const TaskDescription = () => {
+
     return (
         <div className="min-h-screen px-3">
-            <h1 className="text-2xl md:text-3xl text-main mt-4">Task 1</h1>
+            <h1 className="text-2xl md:text-3xl text-main mt-7">Task 1</h1>
             <p className="my-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo fugit nemo similique ipsa dolor exercitationem aut voluptatibus fugiat, quod hic atque perspiciatis? Quibusdam quae reprehenderit doloribus repellat, quas minima omnis.</p>
+
+            <h1 className="text-xl md:text-2xl  my-2 flex items-center gap-2"><span className="text-main"><FaCalendarAlt /></span> Deadline: 
+                <span className="text-main">
+                    10/12/24
+                </span>
+            </h1>
+            <h1 className="text-xl md:text-2xl  my-2 flex items-center gap-2"><span className="text-main"><IoIosTime /></span> Timer: 
+                <span className="text-main">
+                    10:20:30
+                </span>
+            </h1>
             <div className="flex gap-1">
                 <button className="btn btn-sm md:btn-md py-2  border-b-4 border-b-sub_color_1 hover:bg-sub_color_1 hover:border-sub_color_1 hover:text-white_color hover:px-3">Delete</button>
                 <button className="btn btn-sm md:btn-md py-2  border-b-4 border-b-sub_color_1 hover:bg-sub_color_1 hover:border-sub_color_1 hover:text-white_color hover:px-3">Update</button>
