1e6c615890958968d1261e6ec4212a74e7b6fcc9
ayon121 <borshonsaha1234@gmail.com>
2024-01-09 02:49:51 +0600

    basic time added

--- src/Components/Pages/Dashboard/AllTasks/AllTask.jsx
+++ src/Components/Pages/Dashboard/AllTasks/AllTask.jsx
@@ -2,6 +2,7 @@
 // import PropTypes from 'prop-types';
 
 import { Link } from "react-router-dom";
+import { SiTask } from "react-icons/si";
 
 const AllTask = () => {
 
@@ -9,7 +10,7 @@ const AllTask = () => {
         <Link to={`/dashboard/task/id`}  className="w-full mx-auto hover:py-2">
             <div>
                 <div className="flex gap-2 items-center justify-between  mt-3 bg-main py-2 px-3 rounded-3xl text-white_color">
-                    <h1>Task 1</h1>
+                    <h1 className="flex items-center gap-1"><span className="text-white_color"><SiTask /></span> Task 1</h1>
                     <h1>10:20:30</h1>
                 </div>
             </div>
--- src/Components/Pages/Dashboard/TaskDescription/TaskDescription.jsx
+++ src/Components/Pages/Dashboard/TaskDescription/TaskDescription.jsx
@@ -1,10 +1,29 @@
 
 // import PropTypes from 'prop-types';
+import { useEffect, useRef, useState } from "react";
 import { FaCalendarAlt } from "react-icons/fa";
 import { IoIosTime } from "react-icons/io";
 
 const TaskDescription = () => {
+    const [time , settime] = useState(0)
+    const [running , setrunning] = useState(true)
 
+    const timer = useRef()
+
+    useEffect(() => {
+        if (running) {
+            timer.current = setInterval(() => {
+                settime((prev) => prev + 1);
+            }, 1000);
+        } else {
+            clearInterval(timer.current);
+        }
+
+        return () => clearInterval(timer.current);
+    }, [running]);
+
+
+    
     return (
         <div className="min-h-screen px-3">
             <h1 className="text-2xl md:text-3xl text-main mt-7">Task 1</h1>
@@ -15,18 +34,31 @@ const TaskDescription = () => {
                     10/12/24
                 </span>
             </h1>
-            <h1 className="text-xl md:text-2xl  my-2 flex items-center gap-2"><span className="text-main"><IoIosTime /></span> Timer: 
+            <h1 className="text-xl md:text-2xl  my-2 flex items-center gap-2"><span className="text-main "><IoIosTime /></span> Timer: 
                 <span className="text-main">
-                    10:20:30
+                    {format(time)}
                 </span>
             </h1>
             <div className="flex gap-1">
+                <button className="btn btn-sm md:btn-md py-2  border-b-4 border-b-sub_color_1 hover:bg-sub_color_1 hover:border-sub_color_1 hover:text-white_color hover:px-3" >{running ? 'Break' : 'Start'}</button>
                 <button className="btn btn-sm md:btn-md py-2  border-b-4 border-b-sub_color_1 hover:bg-sub_color_1 hover:border-sub_color_1 hover:text-white_color hover:px-3">Delete</button>
                 <button className="btn btn-sm md:btn-md py-2  border-b-4 border-b-sub_color_1 hover:bg-sub_color_1 hover:border-sub_color_1 hover:text-white_color hover:px-3">Update</button>
             </div>
 
         </div>
     );
+};
+
+const format = (time) => {
+    let hours = Math.floor(time / 3600);
+    let minutes = Math.floor((time % 3600) / 60);
+    let seconds = Math.floor(time % 60);
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    return hours + ":" + minutes + ":" + seconds;
 };
 
 TaskDescription.propTypes = {
