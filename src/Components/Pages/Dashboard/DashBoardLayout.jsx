3d11d73107f8b78acd51cb7bc8d331163d73458e
ayon121 <borshonsaha1234@gmail.com>
2024-01-09 23:44:42 +0600

    username added

--- src/Components/Pages/Dashboard/DashBoardLayout.jsx
+++ src/Components/Pages/Dashboard/DashBoardLayout.jsx
@@ -17,7 +17,7 @@ const DashBoardLayout = () => {
             {/* dashboard */}
 
             <div className="flex flex-col gap-3 items-center w-1/4 bg-main h-auto justify-between pt-7 border-r-4 border-r-sub_color_1 rounded-r-3xl px-2 ">
-                <h1 className="text-sm md:text-xl lg:text-2xl text-white_color text-center px-2">Ayon WorkShop</h1>
+                <h1 className="text-sm md:text-xl lg:text-2xl text-white_color text-center px-2">{user?.displayName} WorkShop</h1>
                 <hr className="text-white_color px-1 w-full" />
                 <NavLink to="/dashboard"><div className="flex items-center gap-2"><span className="text-white_color text-2xl lg:text-3xl"><RxDashboard /></span><h1 className="text-xl lg:text-2xl text-white_color hidden md:block">Dashboard</h1></div></NavLink>
                 <hr className="text-white_color px-1 w-full" />
